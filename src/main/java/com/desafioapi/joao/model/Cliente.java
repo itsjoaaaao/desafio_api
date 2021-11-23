90cec5f6b0aca73506d0e07ac3a62399319a0f3c
João <87874652+itsjoaaaao@users.noreply.github.com>
2021-11-23 17:25:29 -0300

    desafio api

---
+++ src/main/java/com/desafioapi/joao/model/Cliente.java
@@ -0,0 +1,64 @@
+package com.desafioapi.joao.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+
+@Entity
+public class Cliente {
+	
+	@Column(nullable = false)
+	private String nome;
+	private String sexo;
+	private String email;
+	private String datanasc;
+	private String naturalidade;
+	private String nacionalidade;
+	private String cpf;
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDatanasc() {
+		return datanasc;
+	}
+	public void setDatanasc(String datanasc) {
+		this.datanasc = datanasc;
+	}
+	public String getNaturalidade() {
+		return naturalidade;
+	}
+	public void setNaturalidade(String naturalidade) {
+		this.naturalidade = naturalidade;
+	}
+	public String getNacionalidade() {
+		return nacionalidade;
+	}
+	public void setNacionalidade(String nacionalidade) {
+		this.nacionalidade = nacionalidade;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	
+}
---
+++ src/main/java/com/desafioapi/joao/repository/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.desafioapi.joao.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.desafioapi.joao.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, String>{
+
+}
